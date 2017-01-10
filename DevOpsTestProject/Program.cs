a338a56e452ca8264985f119dfa0a4ae412b02a1
Saravanan Gajendran <Saravanan Gajendran>
2017-01-10 15:25:49 -0600

    New repository initialized and project DevOpsTestProject added to master

---
+++ DevOpsTestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DevOpsTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SG");
+            Console.WriteLine("SG");
+            Console.WriteLine("SG");
+        }
+    }
+}
